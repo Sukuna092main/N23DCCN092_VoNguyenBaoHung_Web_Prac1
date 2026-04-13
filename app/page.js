2bb45aa2349c6ab00aeff6fb9d5df1f473dd54f9
BaoHung <baohungvonguyen2005@gmail.com>
2026-04-13 15:04:42 +0700

    feat: complete blog listing page with responsive grid

--- app/page.js
+++ app/page.js
@@ -1,3 +1,20 @@
-import Image from "next/image";
+import BlogCard from "../components/BlogCard";
 
+async function getPosts() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/");
+  return res.json();
+}
 
+export default async function HomePage() {
+  const posts = await getPosts();
+
+  return (
+    <main className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        {posts.slice(0, 12).map((post) => (
+          <BlogCard key={post.id} post={post} />
+        ))}
+      </div>
+    </main>
+  );
+}
